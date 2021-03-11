5cdd19d19c53ef4a3f5c67dc669a9b1f3f0f16d3
Gonza <62578122+Olivier21200@users.noreply.github.com>
2021-03-11 15:15:55 +0100

    Add files via upload

--- File/User.java
+++ File/User.java
@@ -1,3 +1,4 @@
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -7,7 +8,7 @@ package projet;
 
 /**
  *
- * @author POULET
+ * @author POULET And Olivier
  */
 public class User {
     
@@ -19,11 +20,15 @@ public class User {
     private boolean IsConnecter;
     private boolean IsChief;
     
-    public void User(){
-        
+    public void User(int id)
+    {
+      this.ID=id;
+      this.IsConnecter=false;
+      this.IsChief=false;
     }
     
-    public void Copy(User another){
+    public void Copy(User another)
+    {
         this.ID = another.ID;
         this.Login = another.Login;
         this.PasswordHash = another.PasswordHash;
@@ -33,8 +38,81 @@ public class User {
         this.IsChief = another.IsChief;
     }
     
-    public void Disconnect(){
-        
+    //change le login du login de l'user
+    public void SetLogin(String login)
+    {
+        this.Login=login;
+    }
+    
+    //renvoie la valeur du login
+    public String GetLogin()
+    {
+        return this.Login;
+    }
+    
+    //change la valeur du passwordhash de l'user
+    public void SetPasswordHash(String Passwordhash)
+    {
+        this.PasswordHash=Passwordhash;
+    }
+    
+    //renvoie la valeur du Passwordhash de l'user
+    public String GetPasswordhash()
+    {
+        return this.PasswordHash;
+    }
+    
+    //change le last name de l'user
+    public void SetLastName(String LastName)
+    {
+        this.LastName=LastName;
+    }
+    
+    //renvoie la valeur du lastName
+    public String GetLastName()
+    {
+        return this.LastName;
+    }
+    
+    //change le last name de l'user
+    public void SetFirstName(String FirstName)
+    {
+        this.FirstName=FirstName;
+    }
+    
+    //renvoie la valeur du FirstName
+    public String GetFirstName()
+    {
+        return this.FirstName;
+    }
+    
+     //change l'état isconnected de l'user
+    public void SetIsConnecter(boolean IsConnecter)
+    {
+        this.IsConnecter=IsConnecter;
+    }
+    
+    //renvoie la valeur de getisconnecter
+    public boolean GetIsConnecter()
+    {
+        return this.IsConnecter;
+    }
+    
+    //change l'état de IsChief de l'user
+    public void SetIsChief(boolean IsChief)
+    {
+        this.IsChief=IsChief;
+    }
+    
+    //renvoie la valeur de IsChief
+    public boolean GetIsChief()
+    {
+        return this.IsChief;
+    }
+    
+    public void Disconnect()
+    {
+        this.IsChief=false;
     }
     
 }
