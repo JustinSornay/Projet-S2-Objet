1c1c60f26ff4bff2b7a9ef3ed8774c5dd4d5c38b
Just1Sornay <72704381+Just1Sornay@users.noreply.github.com>
2021-03-27 20:03:06 +0100

    Add files via upload

--- File/Projet/src/projet/MenuNavigation.java
+++ File/Projet/src/projet/MenuNavigation.java
@@ -27,77 +27,76 @@ public class MenuNavigation extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jPanel1 = new javax.swing.JPanel();
-        jButton1 = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
-        jButton3 = new javax.swing.JButton();
+        background = new javax.swing.JPanel();
+        btnParametre = new javax.swing.JButton();
+        btnListeProjet = new javax.swing.JButton();
+        btnCompte = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setPreferredSize(new java.awt.Dimension(1280, 800));
 
-        jPanel1.setBackground(new java.awt.Color(255, 255, 255));
+        background.setBackground(new java.awt.Color(255, 255, 255));
 
-        jButton1.setBackground(new java.awt.Color(255, 255, 255));
-        jButton1.setFont(new java.awt.Font("Agency FB", 0, 36)); // NOI18N
-        jButton1.setForeground(new java.awt.Color(232, 76, 34));
-        jButton1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/projet/imgMenuNavigation/parametres-nav.png"))); // NOI18N
-        jButton1.setText("Paramètres");
-        jButton1.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
+        btnParametre.setBackground(new java.awt.Color(255, 255, 255));
+        btnParametre.setFont(new java.awt.Font("Agency FB", 0, 36)); // NOI18N
+        btnParametre.setForeground(new java.awt.Color(232, 76, 34));
+        btnParametre.setIcon(new javax.swing.ImageIcon(getClass().getResource("/projet/imgMenuNavigation/parametres-nav.png"))); // NOI18N
+        btnParametre.setText("Paramètres");
+        btnParametre.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        btnParametre.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
+                btnParametreActionPerformed(evt);
             }
         });
 
-        jButton2.setBackground(new java.awt.Color(255, 255, 255));
-        jButton2.setFont(new java.awt.Font("Agency FB", 0, 36)); // NOI18N
-        jButton2.setForeground(new java.awt.Color(232, 76, 34));
-        jButton2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/projet/imgMenuNavigation/listeProjet.png"))); // NOI18N
-        jButton2.setText("Liste des projets");
-        jButton2.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
-        jButton2.addActionListener(new java.awt.event.ActionListener() {
+        btnListeProjet.setBackground(new java.awt.Color(255, 255, 255));
+        btnListeProjet.setFont(new java.awt.Font("Agency FB", 0, 36)); // NOI18N
+        btnListeProjet.setForeground(new java.awt.Color(232, 76, 34));
+        btnListeProjet.setIcon(new javax.swing.ImageIcon(getClass().getResource("/projet/imgMenuNavigation/listeProjet.png"))); // NOI18N
+        btnListeProjet.setText("Liste des projets");
+        btnListeProjet.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        btnListeProjet.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton2ActionPerformed(evt);
+                btnListeProjetActionPerformed(evt);
             }
         });
 
-        jButton3.setBackground(new java.awt.Color(255, 255, 255));
-        jButton3.setFont(new java.awt.Font("Agency FB", 0, 36)); // NOI18N
-        jButton3.setForeground(new java.awt.Color(232, 76, 34));
-        jButton3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/projet/imgMenuNavigation/compte.png"))); // NOI18N
-        jButton3.setText("Compte");
-        jButton3.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
-        jButton3.addActionListener(new java.awt.event.ActionListener() {
+        btnCompte.setBackground(new java.awt.Color(255, 255, 255));
+        btnCompte.setFont(new java.awt.Font("Agency FB", 0, 36)); // NOI18N
+        btnCompte.setForeground(new java.awt.Color(232, 76, 34));
+        btnCompte.setIcon(new javax.swing.ImageIcon(getClass().getResource("/projet/imgMenuNavigation/compte.png"))); // NOI18N
+        btnCompte.setText("Compte");
+        btnCompte.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        btnCompte.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton3ActionPerformed(evt);
+                btnCompteActionPerformed(evt);
             }
         });
 
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(jPanel1Layout.createSequentialGroup()
+        javax.swing.GroupLayout backgroundLayout = new javax.swing.GroupLayout(background);
+        background.setLayout(backgroundLayout);
+        backgroundLayout.setHorizontalGroup(
+            backgroundLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(backgroundLayout.createSequentialGroup()
+                .addGroup(backgroundLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(backgroundLayout.createSequentialGroup()
                         .addGap(220, 220, 220)
-                        .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 375, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(btnCompte, javax.swing.GroupLayout.PREFERRED_SIZE, 375, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(54, 54, 54)
-                        .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 375, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(btnParametre, javax.swing.GroupLayout.PREFERRED_SIZE, 375, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(backgroundLayout.createSequentialGroup()
                         .addGap(431, 431, 431)
-                        .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 375, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addComponent(btnListeProjet, javax.swing.GroupLayout.PREFERRED_SIZE, 375, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addContainerGap(256, Short.MAX_VALUE))
         );
-        jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
+        backgroundLayout.setVerticalGroup(
+            backgroundLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(backgroundLayout.createSequentialGroup()
                 .addGap(119, 119, 119)
-                .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 225, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(btnListeProjet, javax.swing.GroupLayout.PREFERRED_SIZE, 225, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(50, 50, 50)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 225, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 225, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGroup(backgroundLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btnCompte, javax.swing.GroupLayout.PREFERRED_SIZE, 225, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btnParametre, javax.swing.GroupLayout.PREFERRED_SIZE, 225, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addContainerGap(181, Short.MAX_VALUE))
         );
 
@@ -105,27 +104,27 @@ public class MenuNavigation extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(background, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(background, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+    private void btnParametreActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnParametreActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jButton1ActionPerformed
+    }//GEN-LAST:event_btnParametreActionPerformed
 
-    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+    private void btnListeProjetActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnListeProjetActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jButton2ActionPerformed
+    }//GEN-LAST:event_btnListeProjetActionPerformed
 
-    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+    private void btnCompteActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCompteActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jButton3ActionPerformed
+    }//GEN-LAST:event_btnCompteActionPerformed
 
     /**
      * @param args the command line arguments
@@ -163,9 +162,9 @@ public class MenuNavigation extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
-    private javax.swing.JButton jButton3;
-    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel background;
+    private javax.swing.JButton btnCompte;
+    private javax.swing.JButton btnListeProjet;
+    private javax.swing.JButton btnParametre;
     // End of variables declaration//GEN-END:variables
 }
